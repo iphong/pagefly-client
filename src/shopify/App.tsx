72aa3c1dea17127b7ff208557955216aa90156e4
Paul Phan <phanminh65@gmail.com>
2018-06-27 01:01:08 +0700

    unstated demo

--- src/shopify/App.tsx
+++ src/shopify/App.tsx
@@ -1,4 +1,4 @@
-import React, {Component, FormEvent, RefObject} from 'react';
+import React, {Component, FormEvent, ReactNode, RefObject} from 'react';
 import IFrame from 'components/IFrame';
 import {Container, Provider, Subscribe} from 'unstated-x';
 
@@ -20,7 +20,9 @@ class App extends Component {
 					<IFrame>
 						<h3>This is demo Element</h3>
 						<div>
-							<Paragraph/>
+							<Paragraph1/>
+							<Paragraph2/>
+							<Paragraph3/>
 						</div>
 					</IFrame>
 
@@ -82,7 +84,8 @@ const createElement = (settings: object) => (Element: any) => {
 
 		}
 
-		componentDidMount() {
+		handleMouseDown = (e: MouseEvent) => {
+			console.log('mouse down', this)
 			SelectedContainer.setState({
 				selected: this
 			})
@@ -93,7 +96,9 @@ const createElement = (settings: object) => (Element: any) => {
 				<Subscribe to={[this.container]}>
 					{container => {
 						console.log(1111, container.state)
-						return <Element {...container.state} container={container}/>
+						return <Element {...container.state} extraProps={{
+							onMouseDown: this.handleMouseDown
+						}} container={container}/>
 					}}
 				</Subscribe>
 			)
@@ -102,60 +107,126 @@ const createElement = (settings: object) => (Element: any) => {
 	}
 }
 
-const Paragraph = createElement({})(
-	class extends React.Component<{value: string, container: Container<any>}> {
+const createElement1 = (settings: object) => (Element: any) => {
 
-		static defaultProps = {
-			value: 'Hello world!'
+	return class PFElement extends Component<any, any> {
+		container: any
+		constructor(props: {
+			data: object
+		}, context: object) {
+			super(props, context)
+			const { data, ...rest } = props
+			const containerState = {
+				...Element.defaultProps,
+				...data
+			}
+
+			console.log(2222, containerState)
+			this.container = new Container(containerState)
+
 		}
 
-		get content() {
-			return this.contentEditable.current
+		handleMouseDown = (e: MouseEvent) => {
+			console.log('mouse down', this)
+			SelectedContainer.setState({
+				selected: this
+			})
 		}
-
-		get container() {
-			return this.props.container
-		}
-
-		lastHtml: string = ''
 
 		componentDidMount() {
-			this.content.innerHTML = this.props.value
+
+			console.log(this.element)
+			this.element.current.setState = this.container.setStateSync.bind(this.container)
 		}
 
-		componentDidUpdate(nextProps: {
-			value: string
-		}) {
-			if (this.props.value !== nextProps.value && !this.content.matches(':focus')) {
-				this.content.innerHTML = nextProps.value
-			}
-		}
-
-		contentEditable: RefObject<HTMLSpanElement> = React.createRef()
-
-		emitChange = (e: FormEvent<HTMLSpanElement>) => {
-			const target: HTMLElement = e.currentTarget
-			const html = target.innerHTML
-			if (html !== this.lastHtml) {
-				this.container.setState({value: html})
-			}
-			this.lastHtml = html
-		}
+		element: RefObject<Component> = React.createRef()
 
 		render() {
 			return (
-				<div>
+				<Subscribe to={[this.container]}>
+					{container => {
+						return <Element  extraProps={{
+							onMouseDown: this.handleMouseDown
+						}} ref={this.element} {...container.state} container={container}/>
+					}}
+				</Subscribe>
+			)
+		}
+
+	}
+}
+
+class Paragraph extends React.Component<{value: string, extraProps: object, container: Container<any>}> {
+
+	static defaultProps = {
+		value: 'Hello world!'
+	}
+
+	get content() {
+		return this.contentEditable.current
+	}
+
+	get container() {
+		return this.props.container
+	}
+
+	lastHtml: string = ''
+
+	componentDidMount() {
+		this.content.innerHTML = this.props.value
+	}
+
+	componentDidUpdate(nextProps: {
+		value: string
+	}) {
+		if (this.props.value !== nextProps.value && !this.content.matches(':focus')) {
+			this.content.innerHTML = nextProps.value
+		}
+	}
+
+	contentEditable: RefObject<HTMLSpanElement> = React.createRef()
+
+	emitChange = (e: FormEvent<HTMLSpanElement>) => {
+		const target: HTMLElement = e.currentTarget
+		const html = target.innerHTML
+		if (html !== this.lastHtml) {
+			this.container.setState({value: html})
+		}
+		this.lastHtml = html
+	}
+
+	render() {
+		return (
+			<div {...this.props.extraProps}>
 				  <span
 					  contentEditable
 					  ref={this.contentEditable}
 					  onInput={this.emitChange}
 					  onBlur={this.emitChange}
 				  />
-				</div>
-			)
-		}
+			</div>
+		)
+	}
+}
+
+class ParagraphTest extends React.Component<{value: string, extraProps: object}> {
+
+	static defaultProps = {
+		value: 'Hello'
 	}
 
-)
+	render() {
+		console.log(this.state, this.props)
+		return (
+			<div {...this.props.extraProps}>
+				  <input value={this.props.value || 'test'} onChange={e => this.setState({value: e.target.value})}/>
+			</div>
+		)
+	}
+}
+
+const Paragraph1 = createElement({})(Paragraph)
+const Paragraph2 = createElement1({})(Paragraph)
+const Paragraph3 = createElement1({})(ParagraphTest)
 
 export default App;
